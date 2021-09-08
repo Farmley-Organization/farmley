d4c2af22a29b87f0943085678d9b48f8e72e6b4f
Sunny Shubham <sunny.shubham@farmley.com>
2021-09-08 11:02:03 +0530

    New push

--- farmley/farmley/api.py
+++ farmley/farmley/api.py
@@ -1,10 +1,8 @@
 from __future__ import unicode_literals
-import logging
-from pprint import pprint
-import frappe
-import json
 import collections
+import json
 import requests
+import frappe
 
 
 @frappe.whitelist()
@@ -29,25 +27,6 @@ def product_details_for_website(productCategory="", productName=""):
         products_json.append(d)
     return products_json
 
-
-@frappe.whitelist()
-def sales_order():
-    return frappe.db.sql("")
-
-
-# payload = {"order_type": "Sales",
-#            "customer_name": "32+Degree+Studio",
-#            "customer": "A. K. Trading",
-#            "customer_address": "Head Office-Maharashtra-400015-Shipping",
-#            "transaction_date": "2021-09-07",
-#            "items": [{
-#                "item_code": "Almonds-1-10",
-#                "item_name": "Premium Almond Blanched Sliced Farmley Carton Box 15KG",
-#                "delivery_date": "2021-09-07",
-#                "qty": "1",
-#                "rate": "230"
-#            }]
-#            }
 
 @frappe.whitelist()
 def save_orders(customer, customerAddress, transactionDate, itemCode, itemName, deliveryDate, qty, rate, source):
@@ -79,36 +58,5 @@ def save_orders(customer, customerAddress, transactionDate, itemCode, itemName, 
     return save_orders_json_data, tag_json_data
 
 
-##create customer
-
-@frappe.whitelist()
-def save_customer():
-    url1 = "http://localhost:8000/api/resource/Customer"
-    header1 = {"Authorization": "Token 9e820d1621292f3:e40525854287561",
-               "Accept": "application/json",
-               "Content-Type": "application/json"}
-
-    body = {
-        "data": {
-            "customer_name": "dexciss"
-        }
-
-    }
-    r1 = requests.post(url=url1, headers=header1, data=json.dumps(body))
-    json_data = json.loads(r1.content.decode('utf-8'))
-
-    return r1, json_data
 
 
-@frappe.whitelist()
-def rough():
-    url1 = "http://localhost:8000/api/method/frappe.desk.doctype.tag.tag.add_tag"
-    header1 = {"Authorization": "Token 9e820d1621292f3:e40525854287561",
-               "Accept": "application/json",
-               "Content-Type": "application/json"}
-
-    tag_payload = {"tag": "website", "dt": "Sales Order", "dn": "SAL-ORD-2021-00017"}
-    r1 = requests.post(url=url1, headers=header1, data=json.dumps(tag_payload))
-    json_data = json.loads(r1.content.decode('utf-8'))
-
-    return r1, json_data
